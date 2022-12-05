65ae1c66115784144e179491e5fcd0c3eec6a0bd
hsy5490 <hsy5490@naver.com>
2022-12-05 17:37:35 +0900

    javascript_home-contact

--- main.js
+++ main.js
@@ -34,7 +34,19 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth" });
+    // console.log(event.target.dataset.link);
+    // const scrollTo = document.querySelector(link);
+    // scrollTo.scrollIntoView({behavior: "smooth" });
+    scrollIntoView(link);
+
 })
+
+const button=document.querySelector(".home__contact");
+button.addEventListener("click", ()=>{
+    scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector){
+    const scrollTo=document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+}
