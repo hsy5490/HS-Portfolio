748903b7ed2d2907ce25eeb690084427d7665159
hsy5490 <hsy5490@naver.com>
2022-12-05 22:15:40 +0900

    hamburger_수정

--- main.js
+++ main.js
@@ -12,9 +12,6 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
-
-
-
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth' });
@@ -47,7 +44,7 @@ const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
-    console.log(1 - window.scrollY / homeHeight)
+    // console.log(1 - window.scrollY / homeHeight)
 });
 
 // 탑 스크롤 효과1;
@@ -60,25 +57,3 @@ document.querySelector(".top").addEventListener("click", (e) => {
     });
 });
 
-//projects
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
-    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-    if (filter === null) {
-        return;
-    }
-
-    projectContainer.classList.add('anim-out')
-    setTimeout(() => {
-        projects.forEach((project) => {
-            if (filter === '*' || filter === project.dataset.type) {
-                project.classList.remove('invisible');
-            } else {
-                project.classList.add('invisible');
-            }
-        });
-        projectContainer.classList.remove('anim-out');
-    }, 300);
-})
